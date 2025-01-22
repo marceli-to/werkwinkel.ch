d869bf0f15e3c8383760594c8dfed189e542160d
Marcel Stadelmann <m@marceli.to>
2025-01-22 14:49:10 +0100

    wip

--- resources/views/components/media/image.blade.php
+++ resources/views/components/media/image.blade.php
@@ -2,17 +2,22 @@
 <figure class="{{ $class }}">
   <picture>
     @if (in_array('xl', $sizes))
+      <source media="(min-width: 1440px)" srcset="/media/img/{{ $image }}-xl.avif" type="image/avif">
       <source media="(min-width: 1440px)" srcset="/media/img/{{ $image }}-xl.jpg">
     @endif
     @if (in_array('lg', $sizes))
+      <source media="(min-width: 1440px)" srcset="/media/img/{{ $image }}-lg.avif" type="image/avif">
       <source media="(min-width: 1280px)" srcset="/media/img/{{ $image }}-lg.jpg">    
     @endif
     @if (in_array('md', $sizes))
+      <source media="(min-width: 1440px)" srcset="/media/img/{{ $image }}-md.avif" type="image/avif">
       <source media="(min-width: 1024px)" srcset="/media/img/{{ $image }}-md.jpg">    
     @endif
     @if (in_array('sm', $sizes))
+      <source media="(min-width: 1440px)" srcset="/media/img/{{ $image }}-sm.avif" type="image/avif">
       <source media="(min-width: 768px)" srcset="/media/img/{{ $image }}-sm.jpg">    
     @endif
+    <source media="(min-width: 1440px)" srcset="/media/img/{{ $image }}-xs.avif" type="image/avif">
     <source srcset="/media/img/{{ $image }}-xs.jpg">
     <img 
       src="/media/img/{{ $image }}-xs.jpg" 
@@ -20,6 +25,7 @@
       title="{{ $alt }}" 
       height="{{ $width }}" 
       width="{{ $height }}"
-      class="block w-full h-auto">
+      class="block w-full h-auto"
+      loading="lazy">
   </picture>
 </figure>
