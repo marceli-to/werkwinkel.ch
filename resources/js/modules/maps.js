60f5340a64e49b1bacfd23b14ffe2c4b27a149a4
Marcel Stadelmann <m@marceli.to>
2025-01-23 10:03:15 +0100

    wip

--- resources/js/modules/maps.js
+++ resources/js/modules/maps.js
@@ -3,7 +3,7 @@ const initMap = () => {
   var map = new mapboxgl.Map({
       container: 'map',
       style: 'mapbox://styles/marcelitoooo/ck16ms7m51nlo1cmwnqrbjuyq?optimize=true',
-      center: [8.799862179703089, 47.25841286180372],
+      center: [8.79958356703086, 47.258948004965205],
       zoom: 14
   });
   map.addControl(new mapboxgl.NavigationControl());
@@ -15,7 +15,7 @@ const initMap = () => {
       type: 'Feature',
       geometry: {
         type: 'Point',
-        coordinates: [8.799862179703089, 47.25841286180372]
+        coordinates: [8.79958356703086, 47.258948004965205]
       },
       properties: {
         title: 'Glattwiesen-/Grosswiesenstrasse',
